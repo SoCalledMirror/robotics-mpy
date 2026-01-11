798651e7a638b01881f1e395a19e75a03d145130
Kirill Chuvilin <chuvilin@socalled.link>
2026-01-11 03:43:01 +0300

    [documentation] Add socalled/__init__.py. #TSKRBTCSMP-19
    Signed-off-by: Kirill Chuvilin <chuvilin@socalled.link>

---
+++ socalled/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2026 SoCalled Robotics Contributors <community@socalled.link>
+# SPDX-License-Identifier: MPL-2.0
+
+__author__ = "SoCalled Robotics (MicroPython) Contributors <community@socalled.link>"
+__license__ = 'MPL-2.0'
+__version__ = "0.1.0"
